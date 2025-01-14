ffeb934b5be8dc338e5872f8efb4b9faa7a4f488
samuel0004park <samuel0004park@gmail.com>
2025-01-14 16:03:59 +0900

    Initial Commit

---
+++ cmd/dis_upload/dis_upload.go
@@ -0,0 +1,47 @@
+package dis_upload
+
+import (
+	"strings"
+
+	"github.com/rclone/rclone/cmd"
+	"github.com/rclone/rclone/fs/dis_operations"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	cmd.Root.AddCommand(commandDefinition)
+}
+
+var commandDefinition = &cobra.Command{
+	Use:   "dis_upload source:path",
+	Short: `Upload source file via distributing it to registered remotes.`,
+	Long: strings.ReplaceAll(
+		`Upload source file via distributing it to registered remotes. This 
+means selecting a source file in local path and partioning it to several binary 
+files. This is achieved using Erasure Coding so even when some of the partioned 
+blocks are lost, parity blocks can be used to restore the original file.
+
+Note that this command is to obtain a Private Cloud Storage where a single
+file is completely unreadable, thus staying hidden from the storage service 
+provider.
+
+Distributed files will be further encoded and stored in specific directories 
+(only for containing distributed data) in their appropriate remote. 
+The distribution process will select all remotes accessible at the time of
+call and distribute the files using a fair Load Balancing Algorihtm. 
+
+Uploading duplicate files will enact CLI to start an interactive process that
+will ask the user whether to overwrite the file or to skip uploading it. 
+
+If you wish to simply copy the file without any distribution, use the 
+[copy] (/commands/copy/) command instead.`, "|", "`"),
+	Annotations: map[string]string{
+		"groups": "Copy,Filter,Listing,Important",
+	},
+	Run: func(command *cobra.Command, args []string) {
+		cmd.CheckArgs(1, 1, command, args)
+		cmd.Run(true, true, command, func() error {
+			return dis_operations.Dis_Upload(args)
+		})
+	},
+}
---
+++ fs/dis_operations/dis_operations.go
@@ -0,0 +1,75 @@
+package dis_operations
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/rclone/rclone/cmd"
+	"github.com/rclone/rclone/fs/config"
+	"github.com/rclone/rclone/fs/operations"
+	"github.com/rclone/rclone/fs/sync"
+	"github.com/spf13/cobra"
+)
+
+var remoteDirectory = "Distribution"
+
+func Dis_Upload(args []string) (err error) {
+
+	remotes := config.GetRemoteNames()
+
+	for _, remote := range remotes {
+		dest := fmt.Sprintf("%s:%s", remote, remoteDirectory)
+		tempArgs := []string{args[0], dest}
+
+		fmt.Printf("Uploading to remote: %s\n", dest)
+		err = remoteCallCopy(tempArgs)
+
+		if err != nil {
+			return fmt.Errorf("error in Dis_Upload: %w", err)
+		}
+	}
+
+	fmt.Printf("Completed Dis_Upload\n")
+	return nil
+}
+
+func remoteCallCopy(args []string) (err error) {
+	fmt.Printf("Calling remoteCallCopy with args: %v\n", args)
+
+	// Fetch the copy command
+	copyCommand := *commandDefinition
+	copyCommand.SetArgs(args)
+
+	err = copyCommand.Execute()
+	if err != nil {
+		fmt.Printf("Error executing command: %v\n", err)
+		return fmt.Errorf("error executing copyCommand: %w", err)
+	}
+
+	return nil
+}
+
+var (
+	createEmptySrcDirs = false
+)
+
+var commandDefinition = &cobra.Command{
+	Use: "copy source:path dest:path",
+	Annotations: map[string]string{
+		"groups": "Copy,Filter,Listing,Important",
+	},
+	Run: func(command *cobra.Command, args []string) {
+		cmd.CheckArgs(2, 2, command, args)
+		fsrc, srcFileName, fdst := cmd.NewFsSrcFileDst(args)
+		cmd.Run(true, true, command, func() error {
+			if srcFileName == "" {
+				err := sync.CopyDir(context.Background(), fdst, fsrc, createEmptySrcDirs)
+				fmt.Printf("sync.CopyDir completed with error: %v\n", err)
+				return err
+			}
+			err := operations.CopyFile(context.Background(), fdst, fsrc, srcFileName, srcFileName)
+			fmt.Printf("operations.CopyFile completed with error: %v\n", err)
+			return err
+		})
+	},
+}
