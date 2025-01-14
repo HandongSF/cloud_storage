459cb6a6ad66a1cdb1633aa94804c13053189e6f
seongju1015 <gmrfkels1015@gmail.com>
2025-01-14 16:58:40 +0900

    로컬에 파일 있는지 체크

--- fs/dis_operations/dis_operations.go
+++ fs/dis_operations/dis_operations.go
@@ -3,6 +3,9 @@ package dis_operations
 import (
 	"context"
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/rclone/rclone/cmd"
 	"github.com/rclone/rclone/fs/config"
@@ -67,4 +70,33 @@ var commandDefinition = &cobra.Command{
 			return operations.CopyFile(context.Background(), fdst, fsrc, srcFileName, srcFileName)
 		}, true)
 	},
+}
+
+func dis_init(args []string) {
+	path, err := os.Getwd()
+	if err != nil {
+		fmt.Println("error to get current directory path: ", err)
+		return
+	}
+
+	fullPath := filepath.Join(path, args[0])
+	// 존재하지 않는 파일이라면 cmd창에 에러 메세지 출력
+	fi, err := os.Open(fullPath)
+	if err != nil {
+		fmt.Println("file does not exit", err)
+		return
+	}
+	// 존재한다면 ok 메세지 cmd창에 출력
+	fmt.Println("Success to find file : ", fi)
+
+	// 유저가 현재 위치한 로컬 디렉토리에(path) 파일이름으로 디렉토리 생성
+	fileBase := strings.TrimSuffix(args[0], filepath.Ext(args[0]))
+	dirPath := filepath.Join(path, fileBase+"_dir")
+
+	err = os.Mkdir(dirPath, 0755)
+	if err != nil {
+		fmt.Println("Error creating directory: ", err)
+		return
+	}
+	fmt.Println("Directory created successfully at: ", dirPath)
 }
