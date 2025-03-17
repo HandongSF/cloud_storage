9dce27dae1130722a16318a0e2d96312be767f92
samuel0004park <samuel0004park@gmail.com>
2025-03-17 20:36:27 +0900

    Updated load balancer code to call RoundRobin when no throughput info exists

--- cmd/dis_upload/dis_upload.go
+++ cmd/dis_upload/dis_upload.go
@@ -14,7 +14,7 @@ var loadBalancer LoadBalancerFlag
 func init() {
 	cmd.Root.AddCommand(commandDefinition)
 	loadBalancer.Value = dis_operations.RoundRobin // Default value
-	commandDefinition.Flags().VarP(&loadBalancer, "loadbalancer", "b", "Load balancing strategy (RoundRobin, LeastConnections,ResourceBased)")
+	commandDefinition.Flags().VarP(&loadBalancer, "loadbalancer", "b", "Load balancing strategy (RoundRobin, DownloadOptima, UploadOptima, ResourceBased)")
 }
 
 var commandDefinition = &cobra.Command{
